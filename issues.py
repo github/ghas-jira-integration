074e4b65a951f1161f3a2966169c2095e60f6284
John Lugton <john@semmle.com>
2020-12-11 14:39:39 -0800

    GHAS/JIRA integration

--- issues.py
+++ issues.py
@@ -5,208 +5,144 @@ import requests
 import json
 import hashlib
 import hmac
+import logging
+from jira import JIRA
 
-LGTM_URL = os.getenv("LGTM_WEBHOOK_URL")
-assert LGTM_URL != None
 
-GITHUB_URL = os.getenv("GIT_REPO_URL")
-assert GITHUB_URL != None
-
-GITHUB_BOT_USERNAME = os.getenv("GIT_BOT_USERNAME")
-assert GITHUB_BOT_USERNAME != None
-
-GITHUB_TOKEN = os.getenv("GIT_ACCESS_TOKEN")
-assert GITHUB_TOKEN != None
-
-KEY = os.getenv("SECRET", "").encode("utf-8")
+KEY = os.getenv("SECRET_TOKEN", "").encode("utf-8")
 assert KEY != "".encode("utf-8")
 
-sess_github = requests.Session()
-sess_github.headers.update(
-    {"content-type": "application/json", "Authorization": "Bearer %s" % GITHUB_TOKEN}
-)
+JIRA_URL = os.getenv("JIRA_URL")
+assert JIRA_URL != None
 
-sess_lgtm = requests.Session()
-sess_lgtm.headers.update({"content-type": "application/json"})
+JIRA_USERNAME = os.getenv("JIRA_USERNAME")
+assert JIRA_USERNAME != None
 
-SUPPRESSION_LABEL = "wontfix"
+JIRA_PASSWORD = os.getenv("JIRA_PASSWORD")
+assert JIRA_PASSWORD != None
+
+JIRA_PROJECT = os.getenv("JIRA_PROJECT")
+assert JIRA_PROJECT != None
+
+# May need to be changed depending on JIRA project type
+CLOSE_TRANSITION = "Done"
+REOPEN_TRANSITION = "To Do"
+
+jira = JIRA(JIRA_URL, auth=(JIRA_USERNAME, JIRA_PASSWORD))
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
 
-def get_issue_dict(alert, project):
-    """Generate payload for creating ticket in GitHub Issues"""
-
-    title = "%s (%s)" % (alert["query"]["name"], project["name"])
-
-    lines = []
-    lines.append("[%s](%s)" % (alert["query"]["name"], alert["query"]["url"]))
-    lines.append("")
-    lines.append("In %s:" % alert["file"])
-    lines.append("> " + "\n> ".join(alert["message"].split("\n")))
-    lines.append("[View alert on LGTM](%s)" % alert["url"])
-
-    labels = ["LGTM"]
-
-    if alert.get("suppressed"):
-        labels.append(SUPPRESSION_LABEL)
-
-    return {"title": title, "body": "\n".join(lines), "labels": labels}
-
-
-def auth_is_valid(signature):
+def auth_is_valid(signature, request_body):
     if app.debug:
         return True
-
     return hmac.compare_digest(
-        signature, hmac.new(KEY, request.data, hashlib.sha1).hexdigest()
+        signature.encode('utf-8'), 'sha256=' + hmac.new(KEY, request_body, hashlib.sha256).hexdigest().encode('utf-8')
     )
 
+DESC_TEMPLATE="""
+{rule_desc}
 
-@app.route("/lgtm", methods=["POST"])
-def lgtm_webhook():
-    """Handle POST requests coming from LGTM, and pass a translated request to GitHub"""
+{alert_url}
 
-    if not auth_is_valid(request.headers.get("X-LGTM-Signature", "not-provided")):
+----
+This issue was automatically generated from a GitHub alert, and will be automaticall resolved once underlying problem is fixed.
+DO NOT MODIFY DESCRIPTION BELOW LINE.
+GH_ALERT_LOOKUP={repo_name}/code_scanning/{alert_num}
+"""
+
+@app.route("/github", methods=["POST"])
+def github_webhook():
+    """Handle POST requests coming from GitHub, and pass a translated request to JIRA"""
+
+    if not auth_is_valid(request.headers.get("X-Hub-Signature-256", "not-provided"), request.data):
         return jsonify({"code": 403, "error": "Unauthorized"}), 403
+
+    if request.headers.get("X-GitHub-Event", "") != "code_scanning_alert":
+        return jsonify({"code": 400, "error": "Wrong event type: " + request.headers.get("X-GitHub-Event", "")}), 400
 
     json_dict = request.get_json()
 
-    transition = json_dict.get("transition")
+    transition = json_dict.get("action")
+    alert_url = json_dict.get("alert").get("html_url")
+    alert_num = json_dict.get("alert").get("number")
+    rule_id = json_dict.get("alert").get("rule").get("id")
+    rule_desc = json_dict.get("alert").get("rule").get("id")
+    repo_name = json_dict.get("repository").get("full_name")
 
-    # we deal with each transition type individually, showing the expected
+    app.logger.info('Received {action} for {alert_url}'.format(action=transition, alert_url=alert_url))
+
+    # we deal with each action type individually, showing the expected
     # behaviour and response codes explicitly
+    if transition == "appeared_in_branch":
+        app.logger.info('Nothing to do for appeared_in_branch')
+        return jsonify({}), 200
 
-    if transition == "create":
-
-        data = get_issue_dict(json_dict.get("alert"), json_dict.get("project"))
-
-        r = sess_github.post(GITHUB_URL, data=json.dumps(data))
-
-        issue_id = str(r.json().get("number", None))
-
-        if r.ok:
-            return jsonify({"issue-id": issue_id}), 201
-        else:
-            return jsonify({"code": 500, "error": "Internal server error"}), 500
+    if transition == "created":
+        app.logger.info('Creating new issue')
+        jira_issue = jira.create_issue(
+            project=JIRA_PROJECT,
+            summary='{rule} in {repo}'.format(rule=rule_id, repo=repo_name),
+            description=DESC_TEMPLATE.format(
+                rule_desc=rule_desc,
+                alert_url=alert_url,
+                repo_name=repo_name,
+                alert_num=alert_num,
+            ),
+            issuetype={'name': 'Bug'}
+        )
+        app.logger.info('Created issue ' + jira_issue.key)
+        return jsonify({}), 200
 
     # if not creating a ticket, there should be an issue_id defined
-    issue_id = json_dict.get("issue-id", None)
-    if issue_id is None:
-        return jsonify({"code": 400, "error": "No issue-id provided"}), 400
+    issue_search = 'project={jira_project} and description ~ "\\"GH_ALERT_LOOKUP={repo_name}/code_scanning/{alert_num}\\""'.format(
+        jira_project=JIRA_PROJECT,
+        repo_name=repo_name,
+        alert_num=alert_num,
+    )
+    app.logger.info('Searching for issue to update: ' + issue_search)
+    existing_issues = jira.search_issues(issue_search)
 
-    if transition == "close":
+    if not existing_issues:
+        app.logger.error('No issues found for query: ' + issue_search)
+        return jsonify({"code": 400, "error": "Issue not found"}), 400
 
-        r = sess_github.patch(
-            GITHUB_URL + "/" + str(issue_id), data=json.dumps({"state": transition})
-        )
-        if r.ok:
-            return jsonify({"issue-id": issue_id}), 200
-        if r.status_code == 404 and r.json()["message"] == "Not Found":
-            # if we were trying to close, we don't worry about not finding it
-            return jsonify({"issue-id": issue_id}), 200
+    if len(existing_issues) > 1:
+        app.logger.warning('Multiple issues found for: ' + issue_search + '. Selecting by min id.')
+        existing_issues.sort(key=lambda i: i.id)
+    
+    issue = existing_issues[0]
+    app.logger.info('Found issue to update: ' + issue.key)
+    
+    jira_transitions = {t['name'] : t['id'] for t in jira.transitions(issue)}
+
+    if transition in ["closed_by_user", "fixed"] :
+        if CLOSE_TRANSITION not in jira_transitions:
+            app.logger.error('Transition "{close_transition}" not available for {issue_key}. Valid transition: {jira_transitions}'.format(
+                close_transition=CLOSE_TRANSITION,
+                issue_key=issue.key,
+                jira_transitions=list(jira_transitions)
+            ))
+            return jsonify({"code": 400, "error": "Close transition not found"}), 400
         else:
-            return jsonify({"code": 500, "error": "Internal server error"}), 500
+            jira.transition_issue(issue, jira_transitions[CLOSE_TRANSITION])
+            return jsonify({}), 200
 
-    if transition == "reopen":
-
-        r = sess_github.patch(
-            GITHUB_URL + "/" + str(issue_id), data=json.dumps({"state": "open"})
-        )
-        if r.ok:
-            return jsonify({"issue-id": issue_id}), 200
-        if r.status_code == 404 and r.json()["message"] == "Not Found":
-            # using 410 we indicate to LGTM that the issue needs to be recreated
-            return jsonify({"code": 410, "error": "Ticket has been removed."}), 410
+    if transition in ["reopened_by_user", "reopened"]:
+        if REOPEN_TRANSITION not in jira_transitions:
+            app.logger.error('Transition "{reopen_transition}" not available for {issue_key}. Valid transition: {jira_transitions}'.format(
+                reopen_transition=REOPEN_TRANSITION,
+                issue_key=issue.key,
+                jira_transitions=list(jira_transitions)
+            ))
+            return jsonify({"code": 400, "error": "Close transition not found"}), 400
         else:
-            return jsonify({"code": 500, "error": "Internal server error"}), 500
-
-    if transition == "suppress":
-
-        r = sess_github.post(
-            GITHUB_URL + "/" + str(issue_id) + "/" + "labels",
-            data=json.dumps([SUPPRESSION_LABEL]),
-        )
-        if r.ok:
-            return jsonify({"issue-id": issue_id}), 200
-        if r.status_code == 404 and r.json()["message"] == "Not Found":
-            # if we were trying to suppress, we don't worry about not finding it
-            return jsonify({"issue-id": issue_id}), 200
-        else:
-            return jsonify({"code": 500, "error": "Internal server error"}), 500
-
-    if transition == "unsuppress":
-
-        r = sess_github.delete(
-            "/".join([GITHUB_URL, str(issue_id), "labels", SUPPRESSION_LABEL])
-        )
-
-        # if the label was not present on the issue, we don't let this worry us
-        if not r.ok and r.json().get("message") == "Label does not exist":
-            r.status_code = 200
-
-        if r.ok:  # we ensure that the ticket is open in the issue tracker
-            r = sess_github.patch(
-                GITHUB_URL + "/" + str(issue_id), data=json.dumps({"state": "open"})
-            )
-
-        if r.ok:
-            return jsonify({"issue-id": issue_id}), 200
-        if r.status_code == 404 and r.json()["message"] == "Not Found":
-            # using 410 we indicate to LGTM that the issue needs to be recreated
-            return jsonify({"code": 410, "error": "Ticket has been removed."}), 410
-        else:
-            return jsonify({"code": 500, "error": "Internal server error"}), 500
+            jira.transition_issue(issue, jira_transitions[REOPEN_TRANSITION])
+            return jsonify({}), 200
 
     # when the transition is not recognised, we return a bad request response
     return (
         jsonify({"code": 400, "error": "unknown transition type - %s" % transition}),
         400,
     )
-
-
-@app.route("/github", methods=["POST"])
-def github_webhook():
-    """Handle POST requests coming from GitHub, and pass a translated request to LGTM"""
-
-    if not auth_is_valid(
-        request.headers.get("X-Hub-Signature", "not-provided").split("=")[-1]
-    ):
-        return jsonify({"code": 403, "error": "Unauthorized"}), 403
-
-    json_dict = request.get_json()
-
-    action = json_dict.get("action")
-
-    if action not in ["labeled", "unlabeled"]:
-        return jsonify({"status": 200}), 200
-
-    label = json_dict.get("label", {})
-
-    if label.get("name", "") != SUPPRESSION_LABEL:
-        return jsonify({"status": 200}), 200
-
-    issue_id = str(json_dict["issue"]["number"])
-
-    # When we were responsible for changing the tag, we don't want to pass the webhook back again.
-    if json_dict["sender"]["login"] == GITHUB_BOT_USERNAME:
-        return jsonify({"status": 200}), 200
-
-    translator = {"labeled": "suppress", "unlabeled": "unsuppress"}
-
-    payload = json.dumps({"issue-id": issue_id, "transition": translator[action]})
-
-    headers = {
-        "X-LGTM-Signature": hmac.new(
-            KEY, payload.encode("utf-8"), hashlib.sha1
-        ).hexdigest()
-    }
-
-    r = sess_lgtm.post(LGTM_URL, data=payload, headers=headers)
-
-    if r.ok:
-        return jsonify({"status": 200}), 200
-    else:
-        return app.response_class(
-            response=r.content, status=r.status_code, mimetype="application/json"
-        )
